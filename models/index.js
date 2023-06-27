248bd5dd3ce78fb0d546c4c8dd97d7021816a190
KT <132851569+kumih0@users.noreply.github.com>
2023-06-26 21:56:10 -0500

    corrected producttag connection

--- models/index.js
+++ models/index.js
@@ -6,24 +6,32 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category,{
-  foreignKey:'category_id'
+  foreignKey:'category_id',
+  // onDelete:'CASCADE'
 });
 
 // Categories have many Products
-Category.hasMany(Product,{
-  foreignKey:'category_id'
+Category.hasMany(Product,{ 
+  foreignKey:'category_id',
+//   onDelete:'CASCADE'
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag,{
-  through:ProductTag,
-  foreignKey:'product_id'
+  through:{ 
+    model: ProductTag,
+    unique:false
+  },
+  as:'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product,{
-  through:ProductTag,
-  foreignKey:'tag_id'
+  through:{
+    model: ProductTag,
+    unique:false
+  },
+  as:'tag_id'
 });
 
 module.exports = {
